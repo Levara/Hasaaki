69bf15dba16a467cfc960194e008c8c4478b1adb
levara <hrvoje.leventic@gmail.com>
2017-09-09 15:50:52 +0200

    Add image controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Hasaaki::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :galleries 
-  resources :pictures
-
-  root :to => 'galleries#index'
+  root :to => 'images#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
