902c377418a813ff6790bc3ce5723d5951b25946
levara <hrvoje.leventic@gmail.com>
2017-09-11 21:34:07 +0200

    Change job to perform_now for testing

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -9,7 +9,7 @@ class ImagesController < ApplicationController
     image = Image.find(params[:image_id])
     # ruby does not have global scope by default
     # https://stackoverflow.com/a/34655466
-    ::OptimizeSingleJpegJob.perform_later(image) unless image.nil?
+    OptimizeSingleJpegJob.perform_now(image) unless image.nil?
 		redirect_to root_path, notice: "Image optimization started."
   end
     
